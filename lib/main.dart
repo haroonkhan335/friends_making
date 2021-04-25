474ee80b606dc3f11ebfe4698263d00a0f9af458
pdwitharana <pasan@doctors.org.uk>
2021-04-25 20:49:42 +0100

    removed dead code

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  // final data =
-  //     (await FirebaseDatabase.instance.reference().child('user').once()).value;
-  // List<UserModel> users = [];
-  // data.forEach((key, value) {
-  //   users.add(UserModel.fromDocument(value));
-  // });
-
-  // for (final user in users) {
-  //   await FirebaseDatabase.instance
-  //       .reference()
-  //       .child('user/' + user.uid)
-  //       .child('followers')
-  //       .push();
-  // }
-  // await FirebaseAuth.instance.signOut();
+  
   runApp(App());
 }
